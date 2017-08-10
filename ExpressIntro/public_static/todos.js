672378f0d0c0820de69ea35d974fb6a48761b9f6
Nimit Aggarwal <nimitagg95@gmail.com>
2017-08-10 17:00:00 +0530

    express

--- ExpressIntro/public_static/todos.js
+++ ExpressIntro/public_static/todos.js
@@ -7,13 +7,18 @@ $(function () {
 
         let newTodo =  $('#newTodo').val();
         console.log(newTodo);
-        todoList = $('#todolist');
-        
-        $.get('/addTodo?task=' + newTodo, function (data) { // Ajax request
-            todoList.empty();
-            for(todo of data ) {
-                todoList.append(`<li>${todo}</li>`);
-            }
-        });
+        let todoList = $('#todolist');
+        AddTodo(newTodo, todoList);
+
     });
 });
+
+function AddTodo(newTodo,todoList) {
+    $.get('/addTodo?task=' + newTodo, function (data) { // Ajax request
+        todoList.empty();
+        console.log(data);
+        for(todo of data ) {
+            todoList.append(`<li>${todo}</li>`);
+        }
+    });
+}
--- ExpressIntro/server.js
+++ ExpressIntro/server.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
-todos = [];
+var todos = [];
 
 app.get('/', function (req, res, next) {
     res.send('Hello world')
 });
+
+
 
 app.get('/name', function (req, res, next) {
     console.log(req.query);
@@ -21,8 +24,18 @@ app.get('/name', function (req, res, next) {
 app.use('/public', express.static(__dirname + '/public_static'));
 
 app.get('/addTodo', function (req, res, next) {
-    todos.push(req.query.task);
-    res.send(todos);
+    fs.appendFile(__dirname + '/data/data' ,req.query.task+'\n', function (err) {
+        if (err) throw err;
+        fs.readFile(__dirname + '/data/data',function (err,data) {
+            if (err) throw err ;
+             todos = data.toString().split('\n');
+             todos.splice(-1, 1);
+             console.log(todos);
+             res.send(todos);
+        });
+    });
+
+
 });
 
 app.listen(8000);
