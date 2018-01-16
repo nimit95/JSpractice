fe33a8902cb8689a7889ad75968b37680a9ffe7d
nimit95 <nimitagg95@gmail.com>
2018-01-16 21:40:22 +0530

    sockets
    Signed-off-by: nimit95 <nimitagg95@gmail.com>

--- sockets/server.js
+++ sockets/server.js
@@ -1,12 +1,28 @@
 
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 app.use('/', express.static(__dirname + '/public_static'));
 
+io.on('connection', (socket) => {
+
+  console.log('New Client');
+
+  socket.on('new_message', (msg) => {
 
 
-app.listen(5243, function () {
+    io.emit('recvdMsg', msg);
+
+  })
+});
+
+app.get('/hello', (e, s)=> {s.send('hellp')})
+
+server.listen(5243, function () {
   console.log('Server Started on http://localhost:' + 5243);
 });
